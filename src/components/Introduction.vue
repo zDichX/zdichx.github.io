d8fd36cf144bf4611c3255e2d8b4ff0a14cae767
zDich <dich.zh@qq.com>
2024-12-01 17:36:52 +0800

    简单修改了intro

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -19,13 +19,14 @@ onMounted(async () => {
     <p>读作 /ˈzaɪkoʊ/ 或 /ziːˈhaɪkoʊ/，是曾用名 zhyzx 和 空 的结合，也曾叫做 zDich<del>（再叫zDick我跟你爆了</del> </p>
     <p>（也许）是正常人</p>
     <div class="textCtn flex">
-      <div class="text1" data-text="经常出现逆天言论">偶尔上推分享一些零碎的想法</div>  
+      <div class="text1" data-text="经常出现逆天言论（">偶尔上推分享一些零碎的想法</div>  
       <div class="text1" data-text="在中中国人">中文 / English / Google Translate ✔️</div>
-      <div class="text1" data-text="（不是乌蒙吃！！！">移动端音游人 各种音游都玩一点（买不起板子和曲包</div>
+      <div class="text1" data-text="不是乌蒙吃！！！">移动端音游人 各种音游都玩一点（买不起板子和曲包</div>
       <div class="text1"><a href="https://osu.ppy.sh/users/16943258">>>>俄亥俄州立大学休闲玩家>>></a></div>
       <div class="text1" data-text="官zDich#5421，欢迎互7（">玩一款由鹰角网络自主开发运营的策略塔防类手游，但不玩一款开放世界冒险游戏</div>
       <div class="text1" data-text="遥遥领先">看数码的 不会技术</div>
-      <div class="text1" data-text="（你们南通不要太荒谬">LGBTQ+友好</div>
+      <div class="text1" data-text="骂谁罕见">几乎不键政，但是关注/转推可能有</div>
+      <div class="text1" data-text="你们南通不要太荒谬">LGBTQ+友好</div>
       <div class="text1" data-text="自己去pixiv.net解决">不会有NSFW内容</div>
       <div class="text1" data-text="还好根本没人理我😎👌">淡推，有些消息不一定及时回</div>
       <div class="text1" data-text="因为缺乏表达能力（">不太希望交朋友，但是关注回fo（限正常人，且有至少一次发推/转推）</div>
@@ -48,21 +49,21 @@ onMounted(async () => {
       <li>不太希望交朋友，但是关注回fo（限正常人，且有至少一次发推/转推）</li>
     </ul> -->
     <div class="ending flex" ref="udElement">under construction...</div>
-    <div class="mobileText"><del>什么，你不知道每行文字都可以点吗</del></div>
+    <div class="mobileText"><del>什么 你不知道每行文字都可以点吗</del></div>
   </div>
 </template>
 
 <style scoped>
 .introduction {
-  height: 150vh;
+  height: 125vh;
   flex-direction: column;
-  font-size: clamp(0.5rem, 2vw + 0.5rem, 1.5rem);
+  font-size: clamp(1.2rem, 2vw + 0.5rem, 1.5rem);
   overflow: hidden;
   padding: 10px;
 }
 
 .ending {
-  font-size: clamp(1rem, 2vw + 1rem, 2.5rem);
+  font-size: 2.5rem;
   margin: auto 0;
 }
 
@@ -109,6 +110,7 @@ a, a:visited, a:hover {
 .mobileText {
   display: none;
   color: gray;
+  margin-bottom: 30px;
 }
 
 @media (pointer: coarse) {
